90e3bc791c5fa3026c73aa5152e11ba82dae4c66
psm7047 <psm7047@naver.com>
2021-02-16 15:05:10 +0900

    소스 수정 리모트 수정

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -3,7 +3,8 @@ package ch01;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java!");
+		System.out.println("Hello Java1!");
+		System.out.println("Hello Java2!");
 		
 		
 	
