750356f0a11d3e5b4f1ce547ce51c896a02aed8f
psm7047 <psm7047@naver.com>
2021-02-22 15:06:00 +0900

    02/22 오전 수업

---
+++ src/ch06/exam14/Example.java
@@ -0,0 +1,17 @@
+package ch06.exam14;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		//Korean.NATION = "미국";
+		System.out.println(Korean.NATION);
+		
+		Korean k1 = new Korean("123456-1234567"); 
+		//k1.ssn = "234444-4444"; 바꿀 수 없음
+		
+		
+		
+	}
+
+}
---
+++ src/ch06/exam14/Korean.java
@@ -0,0 +1,16 @@
+package ch06.exam14;
+
+public class Korean {
+	
+	//field
+	final static String NATION = "한국";
+	final String ssn ;
+	
+	//constructor
+	Korean(String ssn) {	//final 생성자에서 값 바꿈(한번만)
+		this.ssn = ssn;
+	}
+	
+	//method
+
+}
